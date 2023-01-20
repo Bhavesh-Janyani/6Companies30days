6afbfc1aa492ad888d6bda7791b95cd9937c6616
Bhavesh-Janyani <bhaveshjanjani13@gmail.com>
2023-01-20 21:31:40 +0530

    Solved 10th Question

---
+++ Flipkart/Distant_Barcodes.py
@@ -0,0 +1,12 @@
+# Runtime = 409ms (98.14%) 
+# Memory = 16.1MB (92.9%)
+
+class Solution:
+    def rearrangeBarcodes(self, barcodes: List[int]) -> List[int]:
+        cnt = collections.Counter(barcodes).most_common()[::-1]
+        ref = [val for val, t in cnt for _ in range(t)]
+        for i in range(0, len(barcodes), 2):
+            barcodes[i] = ref.pop()
+        for i in range(1, len(barcodes), 2):
+            barcodes[i] = ref.pop()
+        return barcodes
