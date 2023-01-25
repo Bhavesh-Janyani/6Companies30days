1089210b92c5f6dce0c62293afcd0262fa2f469c
Bhavesh-Janyani <bhaveshjanjani13@gmail.com>
2023-01-25 22:39:40 +0530

    Solved 7th Question

---
+++ Google/K_Divisible_Elements_Subarrays.py
@@ -0,0 +1,18 @@
+# Runtime = 461ms (80.70%) 
+# Memory = 22.2MB (77.63%)
+
+class Solution:
+    def countDistinct(self, nums: List[int], k: int, p: int) -> int:
+        n = len(nums)                        
+        sub_arrays = set()
+        for start in range(n):
+            cnt = 0
+            temp = ''
+            for i in range(start, n):
+                if nums[i]%p == 0:
+                    cnt+=1                 
+                temp+=str(nums[i]) + ','      
+                if cnt>k: 
+                    break
+                sub_arrays.add(temp)                                    
+        return len(sub_arrays)
